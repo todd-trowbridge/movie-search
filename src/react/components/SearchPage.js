889a8f8d1a275ff138340214831636507ad2b358
Todd Trowbridge <84187785+todd-trowbridge@users.noreply.github.com>
2021-08-28 15:27:21 -0400

    test jsonplaceholder https fetch

--- src/react/components/SearchPage.js
+++ src/react/components/SearchPage.js
@@ -33,12 +33,15 @@ function SearchPage() {
   const [searchTerm, setSearchTerm] = useState("");
 
   const handleSubmit = (e) => {
-    let url = `www.omdbapi.com/?apikey=${key}=`;
+    // let url = `www.omdbapi.com/?apikey=${key}=`;
+    let url = "https://jsonplaceholder.typicode.com/todos/"
     e.preventDefault();
-    fetch(encodeURI(url + searchTerm))
+    // fetch(encodeURI(url + searchTerm))
+    fetch(encodeURI(url))
       .then((res) => res.json())
       .then((data) => {
-        setMovies(data.Search);
+        // setMovies(data.Search);
+        console.log(data)
       });
   };
   return (
